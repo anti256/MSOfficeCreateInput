c346be159d70054ff23b859cd9c33d05ed99d3e3
anti256 <anti256@mail.ru>
2023-10-20 14:09:24 +0300

    almost begining

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+
+import com.spire.doc.Document;
+import com.spire.doc.FileFormat;
+import com.spire.doc.Section;
+import com.spire.doc.documents.BuiltinStyle;
+import com.spire.doc.documents.Paragraph;
+import com.spire.doc.documents.ParagraphStyle;
+
 public class Main {
     public static void main(String[] args) {
+        Document document = new Document();
 
+        document.saveToFile(
+                "output/CreateAWordDocument.docx",
+                FileFormat.Docx);
     }
 }
