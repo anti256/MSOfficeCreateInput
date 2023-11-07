85b9fddda62ed6eac4218549627a5fa3254ba514
anti256 <anti256@mail.ru>
2023-11-07 08:32:29 +0300

    can't finded truble with tables borders

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Table;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xwpf.usermodel.*;
@@ -15,6 +16,16 @@ public class Main {
 
     public static String output = "F:\\ProbaJava.docx";
 
+    //установка границ для таблицы - можно и без этого
+    static void setAllBorders(XWPFTable table, XWPFTable.XWPFBorderType borderType, int size, int space, java.lang.String rgbColor) {
+        table.setTopBorder(borderType, size, space, rgbColor);
+        table.setRightBorder(borderType, size, space, rgbColor);
+        table.setBottomBorder(borderType, size, space, rgbColor);
+        table.setLeftBorder(borderType, size, space, rgbColor);
+        table.setInsideHBorder(borderType, size, space, rgbColor);
+        table.setInsideVBorder(borderType, size, space, rgbColor);
+    }
+
     public static void main(String[] args) throws IOException {
 
         //XWPFDocument — целостное представление Word документа.
@@ -24,9 +35,6 @@ public class Main {
         //всё, что будет создано до создания нового параграфа, будет в этом параграфе
         XWPFParagraph title = document.createParagraph();
         title.setAlignment(ParagraphAlignment.LEFT);
-
-
-
 
         //XWPFRun - набор данных о выводе текста внутри параграфа.
         // Находится может только внутри параграфа, создается через вызов метода параграфа-родителя
@@ -48,16 +56,23 @@ public class Main {
 
         //document.createTable().createRow().createCell().addParagraph();
         XWPFTable table = document.createTable(7,5);
-        table.setWidthType(TableWidthType.DXA);
+        setAllBorders(table, XWPFTable.XWPFBorderType.DOUBLE, 4, 0, "FF0000");
+        table.setWidthType(TableWidthType.PCT);//dxa - установка в процентах, nil - устанавливает ширину в ноль
         table.setWidth("100.00%");
-        int wid = table.getRow(1).getCell(1).getWidth();
-        table.setWidth(wid);
+
+        int w = table.getWidth()/5;
+        XWPFTableRow row = table.getRow(1);
+        for (XWPFTableCell cell: row.getTableCells()
+             ) {
+            cell.setWidthType(TableWidthType.DXA);
+            cell.setWidth("" + w);
+        }
 
 
         //делает тоже самое по поводу ширины таблицы
-        //CTTblWidth widthRepr = table.getCTTbl().getTblPr().addNewTblW();
-        //widthRepr.setType(STTblWidth.PCT);
-        //widthRepr.setW("100.00%");//ширина таблицы во всю возможную ширину на странице
+//        CTTblWidth widthRepr = table.getCTTbl().getTblPr().addNewTblW();
+//        widthRepr.setType(STTblWidth.PCT);
+//        widthRepr.setW("100.00%");//ширина таблицы во всю возможную ширину на странице
         //table.getRows().get(3).getCell(4).setText("3/4");
         //table.getRow(1).getCell(0).setWidth("20.0%");
 
@@ -72,3 +87,5 @@ public class Main {
 //                FileFormat.Docx);
     }
 }
+//К сожалению, так и не удалось найти каким образом зафиксировать ширины столбцов. Сделать таблицу с определенными ширинами столбцов не проблема,
+//проблема когда открываешь созданный файл с таблицей в WORD, при редактировании ширины столбцов сдвигаются.
